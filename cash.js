d96371ce22aaab8c2030766f50b4ea0defcc7b4d
Ilya Agarishev <ilyagarishev@gmail.com>
2022-10-03 16:07:08 +0600

    Добавил  Алгоритм кеширования

---
+++ cash.js
@@ -0,0 +1,42 @@
+// Алгоритм кеширования данных во избижании повторных вычеслений
+
+// cashFunction - ф-ия которая кеширует внутри себя результат вычесления другой ф-ии fn
+function cashFunction(fn) {
+  // Объект, который хранит по ключу результат кеширования, ключем будет параметр n который мы передаем в fn
+  const cash = {};
+
+  return (n) => {
+    if (cash[n]) {
+      console.log("Выведено из кеша: ", cash[n]);
+
+      return cash[n];
+    }
+
+    let result = fn(n);
+
+    cash[n] = result;
+
+    console.log("Выведено не из кеша: ", cash[n]);
+
+    return result;
+  };
+}
+
+function factorial(n) {
+  let result = 1;
+  while (n != 1) {
+    result *= n;
+    n -= 1;
+  }
+
+  return result;
+}
+
+const cashFactorial = cashFunction(factorial);
+
+cashFactorial(5);
+cashFactorial(4);
+cashFactorial(3);
+cashFactorial(4);
+cashFactorial(5);
+cashFactorial(1);
